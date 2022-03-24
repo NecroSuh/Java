8796fe39ed8d82410f1d31103472bbb0516a3ffc
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-03-24 14:11:42 +0900

    Add files via upload

---
+++ Ex05_03.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Ex05_03 {
+
+	public static void main(String[] args) {
+		int[] score = { 79, 88, 91, 33, 100, 55, 95 };
+
+		int max = score[0];		// 배열의 첫 번째 값으로 최대값을 초기화 한다.
+		int min = score[0];		// 배열의 첫 번째 값으로 최소값을 초기화 한다.
+
+		for (int i = 1; i < score.length; i++) {
+			if (score[i] > max) {
+				max = score[i];
+			} else if (score[i] < min) {
+				min = score[i];
+			}
+		}
+		System.out.println("최댓값:" + max);
+		System.out.println("최소값 :" + min);
+	}
+}
