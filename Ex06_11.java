5d905c543a116c44d1928da03f7010e97ed6671d
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-04-02 15:41:43 +0900

    Add files via upload

---
+++ Ex06_11.java
@@ -0,0 +1,30 @@
+package class03;
+
+class Car{
+	String color;
+	String gearType;
+	int door;
+	
+	Car(){}
+	
+	Car(String c, String g, int d){
+		color=c;
+		gearType=g;
+		door=d;
+	}
+}
+
+public class Ex06_11 {
+	public static void main(String[] args) {
+		Car c1=new Car();
+		c1.color="white";
+		c1.gearType="auto";
+		c1.door=4;
+		
+		Car c2=new Car("white","auto",4);
+		
+		System.out.println("c1ÀÇ color="+c1.color+", gearType="+c1.gearType+", door="+c1.door);
+		System.out.println("c2ÀÇ color="+c2.color+", gearType="+c2.gearType+", door="+c2.door);
+	}
+
+}
