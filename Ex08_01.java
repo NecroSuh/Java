af700497fdf643c7925a39dcef30f67274292273
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-04-13 22:21:24 +0900

    Add files via upload

---
+++ Ex08_01.java
@@ -0,0 +1,15 @@
+package try01;
+
+public class Ex08_01 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		try {
+			System.out.println(2);
+			System.out.println(3);
+		} catch (Exception e) {
+			System.out.println(4); // 실행되지 않는다.
+		} // try-catch의 끝
+		System.out.println(5);
+	}
+}
