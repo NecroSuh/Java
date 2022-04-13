f082375f06d3f19654729dd8691d8210d4685f56
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-04-13 22:33:36 +0900

    Add files via upload

---
+++ Ex08_03.java
@@ -0,0 +1,18 @@
+package try01;
+
+public class Ex08_03 {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+
+		try {
+			System.out.println(3);
+			System.out.println(0 / 0);
+			System.out.println(4); // 실행되지 않는다.
+		} catch (Exception e) { // ArithmeticException 대신 Exception을 사용
+			System.out.println(5);
+		}
+		System.out.println(6);
+	}
+}
