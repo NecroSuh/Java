7df8bee1d2c0c7ac6b2d03b33e4b36431044aa33
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-05-03 16:56:48 +0900

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,17 +1,22 @@
-import java.util.Scanner; //Scanner 클래스 입력 기능을 제공
+package shape;
 
-public class Circle {
+public class Circle extends Shape {
+	private int r;
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in); // Scanner 객체 생성
-		int rad;
-		double size;
-		final double PHI = 3.1415;
-
-		System.out.print("반지름 : ");
-		rad = scan.nextInt(); // 정수 입력
-		size = rad * rad * PHI;
-		System.out.println("면적 = " + size);
-		System.out.printf("면적 = %.1f\n", size);
+	public Circle(Point p, int r) {
+		super(p);
+		this.r = r;
 	}
+
+	@Override
+	public void draw() {
+		System.out.println("Circle : " + p1 + " - " + r);
+		System.out.println("		size= " + size);
+	}
+
+	@Override
+	public void calc() {
+		size = (int) (r * r * 3.1415);
+	}
+
 }
