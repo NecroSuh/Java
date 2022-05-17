c63b2f98700c553b64c91a525d6605d8e11901ce
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-05-17 16:47:30 +0900

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,12 @@
+package lambda;
+
+// MyInterface를 구현하는 MyClass 정의, MyInterface를 싱속하는 자식클래스 정의, 상속관계 존재.
+
+public class MyClass implements MyInterface {
+	@Override
+	public void sendMsg(String msg) {
+		
+	}
+}
+
+
