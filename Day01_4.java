8828fc0a324ce00d677e2cad0609a184f8cb5374
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-14 23:40:19 +0900

    Add files via upload

---
+++ Day01_4.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class Day01_4 {
+
+	public static void main(String[] args) {
+
+		int[] array = new int[100];
+		for (int i = 0; i < 100; i++) {
+			array[i] = (int) (Math.random() * 100 + 1);
+		}
+		int sum = 0;
+		for (int i = 0; i < 100; i++) {
+			sum += array[i];
+		}
+		System.out.println("100개의 랜덤 정수의 평균 값은 " + sum / 100 + "입니다.");
+	}
+
+}
