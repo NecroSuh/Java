3b80f0066c294252c317bb770f47ee5cd05545fe
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-15 22:52:50 +0900

    Add files via upload

---
+++ Main_S.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class Main_S {
+
+	public static void main(String[] args) {
+		Student student1 = new Student("강호동", 40, 180, 86, "20220715", 1, 4.5);
+		Student student2 = new Student("유재석", 42, 175, 70, "20220714", 4, 4.3);
+		student1.show();
+		student2.show();
+	}
+
+}
