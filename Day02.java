772fe90383b737bb2ca7fb9b809c21f2a3915296
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-15 22:52:15 +0900

    Add files via upload

---
+++ Day02.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class Day02 {
+
+	final static int SECOND = 1000;
+
+	public static void main(String[] args) {
+		int minutes = SECOND / 60;
+		int second = SECOND % 60;
+
+		System.out.println(minutes + "Ка" + second + "УЪ");
+	}
+
+}
