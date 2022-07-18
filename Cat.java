15ace341b97b34f630e51b44c63ea33381205992
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-07-18 22:48:35 +0900

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,10 @@
+package day04;
+
+public class Cat extends Animal{
+
+	@Override
+	void crying() {
+		System.out.println("Mow Mow");
+	}
+	
+}
---
+++ Dog.java
@@ -0,0 +1,10 @@
+package day04;
+
+public class Dog extends Animal{
+
+	@Override
+	void crying() {
+		System.out.println("Wof! Wof!");
+	}
+	
+}
