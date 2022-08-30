e12ef2a8dd3b1f7864e13b57d9b2b3be91629620
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-08-30 22:10:55 +0900

    Add files via upload

---
+++ Ex11_16.java
@@ -0,0 +1,36 @@
+package day06;
+
+import java.util.*;
+
+public class Ex11_16 {
+
+	public static void main(String[] args) {
+		HashMap map = new HashMap();
+		map.put("myId", "1234");
+		map.put("asdf", "1111");
+		map.put("asdf", "1234");// 이미 존재하는 키 추가기능. 기존 값은 없어짐
+
+		Scanner s = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("id와 password를 입력해주세요.");
+			System.out.print("id : ");
+			String id = s.nextLine().trim();
+
+			System.out.print("password : ");
+			String password = s.nextLine().trim();
+			System.out.println();
+
+			if (!map.containsKey(id)) {
+				System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
+				continue;
+			}
+
+			if (!(map.get(id)).equals(password)) {
+				System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+			} else {
+				System.out.println("id와 비밀번호가 일치합니다.");
+			}
+		}
+	}
+}
