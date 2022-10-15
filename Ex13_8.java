1bd0507b85e758bab83af821d0dc59b056b429d1
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-10-15 23:38:57 +0900

    Add files via upload

---
+++ Ex13_8.java
@@ -0,0 +1,33 @@
+package day08;
+
+public class Ex13_8 {
+	public static void main(String[] args) {
+		ThreadEx8_1 th1 = new ThreadEx8_1();
+		ThreadEx8_2 th2 = new ThreadEx8_2();
+		th1.start();
+		th2.start();
+
+		try {
+			th1.sleep(2000);
+		} catch (InterruptedException e) {
+		}
+		System.out.print("<<main 종료>>");
+	}
+}
+
+class ThreadEx8_1 extends Thread {
+	public void run() {
+		for (int i = 0; i < 300; i++) {
+			System.out.print("-");
+		}
+		System.out.print("<<th1 종료>>");
+	}
+}
+
+class ThreadEx8_2 extends Thread {
+	public void run() {
+		for (int i = 0; i < 300; i++)
+			System.out.print("|");
+		System.out.print("<<th2 종료>>");
+	}
+}
