8fa68641b8441d3cf663a49dee7b56ba69bfafbc
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-10-21 23:26:59 +0900

    Add files via upload

---
+++ Ex13_11.java
@@ -0,0 +1,37 @@
+package day08;
+
+public class Ex13_11 {
+	static long startTime = 0;
+
+	public static void main(String[] args) {
+		ThreadEx11_1 th1 = new ThreadEx11_1();
+		ThreadEx11_2 th2 = new ThreadEx11_2();
+		th1.start();
+		th2.start();
+		startTime = System.currentTimeMillis();
+
+		try {
+			th1.join(); // main 쓰레드가 th1의 작업이 끝날 때까지 기다린다.
+			th2.join(); // main 쓰레드가 th2의 작업이 끝날 때까지 기다린다.
+		} catch (InterruptedException e) {
+		}
+		System.out.print("소요시간: " + (System.currentTimeMillis() - Ex13_11.startTime));
+
+	}
+}
+
+class ThreadEx11_1 extends Thread {
+	public void run() {
+		for (int i = 0; i < 300; i++) {
+			System.out.print(new String("-"));
+		}
+	}
+}
+
+class ThreadEx11_2 extends Thread {
+	public void run() {
+		for (int i = 0; i < 300; i++) {
+			System.out.print(new String("|"));
+		}
+	}
+}
