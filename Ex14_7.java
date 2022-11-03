87aed03707e87fd1b4ed0973c27e10b415a799ca
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2022-11-03 23:06:04 +0900

    Add files via upload

---
+++ Ex14_7.java
@@ -0,0 +1,24 @@
+package day09;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Ex14_7 {
+	public static void main(String[] args) {
+		Stream<String[]> strArrStrm = Stream.of(new String[] { "abc", "def", "jkl" },
+				new String[] { "ABC", "GHI", "JKL" });
+
+		// Stream<Stream<String>> strStrmStrm = strArrStrm.map(Arrays::stream);
+		Stream<String> strStrm = strArrStrm.flatMap(Arrays::stream);
+
+		strStrm.map(String::toLowerCase).distinct().sorted().forEach(System.out::println);
+		System.out.println();
+
+		String[] lineArr = { "Believe or not It is true", "Do or do not There is no try", };
+
+		Stream<String> lineStream = Arrays.stream(lineArr);
+		lineStream.flatMap(line -> Stream.of(line.split(" +"))).map(String::toLowerCase).distinct().sorted()
+				.forEach(System.out::println);
+		System.out.println();
+	}
+}
