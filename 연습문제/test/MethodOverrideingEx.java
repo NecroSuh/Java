51b4de9a6e019d60ae0a40435e64fb534a31d5a6
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-03-19 21:55:13 +0900

    Add files via upload

---
+++ 연습문제/test/MethodOverrideingEx.java
@@ -0,0 +1,48 @@
+package test;
+
+class Shape {
+	public Shape next;
+
+	public Shape() {
+		next = null;
+	}
+
+	public void draw() {
+		System.out.println("Shape");
+	}
+}
+
+class Line extends Shape {
+	public void draw() {
+		System.out.println("Line");
+	}
+}
+
+class Rect extends Shape {
+	public void draw() {
+		System.out.println("Rect");
+	}
+}
+
+class Circle2 extends Shape {
+	public void draw() {
+		System.out.println("Circle");
+	}
+}
+
+public class MethodOverrideingEx {
+	static void paint(Shape p) {
+		p.draw();
+	}
+
+	public static void main(String[] args) {
+		Line line = new Line();
+
+		paint(line);
+		paint(new Shape());
+		paint(new Line());
+		paint(new Rect());
+		paint(new Circle2());
+
+	}
+}
