e81c1ff156ddbcdcc0ee5bfdeff50eb4291eb9df
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-03-20 21:18:41 +0900

    Add files via upload

---
+++ 연습문제/four/Ex40.java
@@ -0,0 +1,48 @@
+package four;
+
+import java.util.Scanner;
+
+abstract class Converter {
+	abstract protected double convert(double src);
+
+	abstract protected String getSrcString();
+
+	abstract protected String getDestString();
+
+	protected double ratio;
+
+	public void run() {
+		Scanner s = new Scanner(System.in);
+		System.out.println(getSrcString() + "을 " + getDestString() + "로 바꿉니다.");
+		System.out.print(getSrcString() + "을 입력하세요.>> ");
+		double val = s.nextDouble();
+		double res = convert(val);
+		System.out.println("변환 결과: " + res + getDestString() + "입니다.");
+		s.close();
+	}
+}
+
+class Won2Dollar extends Converter {
+	public Won2Dollar(double ratio) {
+		this.ratio = ratio;
+	}
+
+	protected double convert(double src) {
+		return src / ratio;
+	}
+
+	protected String getSrcString() {
+		return "원";
+	}
+
+	protected String getDestString() {
+		return "달러";
+	}
+}
+
+public class Ex40 {
+	public static void main(String[] args) {
+		Won2Dollar toDollar = new Won2Dollar(1200);
+		toDollar.run();
+	}
+}
