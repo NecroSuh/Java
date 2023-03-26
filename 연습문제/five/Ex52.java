22ba3a72a316d8d3d359d9a6d880989488954356
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-03-26 22:59:34 +0900

    Add files via upload

---
+++ 연습문제/five/Ex52.java
@@ -0,0 +1,22 @@
+package five;
+
+import java.util.Calendar;
+
+public class Ex52 {
+	public static void main(String[] args) {
+		Calendar now = Calendar.getInstance();
+
+		int hour = now.get(Calendar.HOUR_OF_DAY);
+		int minute = now.get(Calendar.MINUTE);
+		System.out.println("현재 시간은 " + hour + "시" + minute + "분입니다.");
+
+		if (hour > 4 && hour < 12)
+			System.out.println("Good Morning");
+		else if (hour >= 12 && hour < 18)
+			System.out.println("Good Afternoon");
+		else if (hour > 18 && hour < 22)
+			System.out.println("Good Evening");
+		else
+			System.out.println("Good Night");
+	}
+}
