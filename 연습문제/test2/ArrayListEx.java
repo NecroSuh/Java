3ca0386c8bc207ab2b3096ad7ae77971f19c7e2a
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-03-30 23:07:45 +0900

    Add files via upload

---
+++ 연습문제/test2/ArrayListEx.java
@@ -0,0 +1,28 @@
+package test2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayListEx {
+	public static void main(String[] args) {
+		ArrayList<String> a = new ArrayList<String>();
+
+		Scanner s = new Scanner(System.in);
+		for (int i = 0; i < 4; i++) {
+			System.out.print("이름을 입력하세요>> ");
+			String st = s.next();
+			a.add(st);
+		}
+		for (int i = 0; i < a.size(); i++) {
+			String name = a.get(i);
+			System.out.print(name + " ");
+		}
+		int longestIndex = 0;
+		for (int i = 1; i < a.size(); i++) {
+			if (a.get(longestIndex).length() < a.get(i).length())
+				longestIndex = i;
+		}
+		System.out.println("\n가장 긴 이름은: " + a.get(longestIndex));
+		s.close();
+	}
+}
