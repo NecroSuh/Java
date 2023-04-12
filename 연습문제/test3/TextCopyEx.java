19bf2a75523600ebcaf72c0f07e76ff31753145b
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-04-12 22:19:23 +0900

    Add files via upload

---
+++ 연습문제/test3/TextCopyEx.java
@@ -0,0 +1,26 @@
+package test3;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TextCopyEx {
+	public static void main(String[] args) {
+		File src = new File("C:\\windows\\system.ini");
+		File dest = new File("C:\\Temp\\system.txt");
+
+		int c;
+		try {
+			FileReader fr = new FileReader(src);
+			FileWriter fw = new FileWriter(dest);
+			while ((c = fr.read()) != -1)
+				fw.write((char) c);
+			fr.close();
+			fw.close();
+			System.out.println(src.getPath() + "를 " + dest.getPath() + "로 복사하였습니다.");
+		} catch (IOException e) {
+			System.out.println("파일 복사 오류");
+		}
+	}
+}
