e667369eab11378158b3e282772f29ce45f66631
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-04-27 23:07:51 +0900

    Add files via upload

---
+++ 연습문제/test4/FlowLayoutEx.java
@@ -0,0 +1,27 @@
+package test4;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class FlowLayoutEx extends JFrame {
+	public FlowLayoutEx() {
+		setTitle("FlowLayout Sample");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+
+		c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+		c.add(new JButton("add"));
+		c.add(new JButton("sub"));
+		c.add(new JButton("mul"));
+		c.add(new JButton("div"));
+		c.add(new JButton("Calculate"));
+
+		setSize(300, 200);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new FlowLayoutEx();
+	}
+}
