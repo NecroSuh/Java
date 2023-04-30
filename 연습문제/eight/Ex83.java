a7fcb0a27e05afc50cdff17d3043b257add70b7c
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-04-30 22:14:04 +0900

    Create Ex83.java

---
+++ 연습문제/eight/Ex83.java
@@ -0,0 +1,18 @@
+package eight;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Ex83 extends JFrame {
+	Ex83() {
+		setTitle("Let's study Java");
+		setSize(400, 200);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Ex83();
+	}
+}
