4b8a85932efb9d0fd8bc43c6122f0d8ffbdd4049
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-05-05 20:34:41 +0900

    Create MouseAdapterEx.java

---
+++ 연습문제/test05/MouseAdapterEx.java
@@ -0,0 +1,38 @@
+package test05;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MouseAdapterEx extends JFrame{
+	private JLabel la=new JLabel("Hello");
+	
+	public MouseAdapterEx() {
+		setTitle("Mouse 이벤트 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c=getContentPane();
+		c.addMouseListener(new MyMouseAdapter());
+		
+		c.setLayout(null);
+		la.setSize(50,50);
+		la.setLocation(30, 30);
+		c.add(la);
+		
+		setSize(250,250);
+		setVisible(true);
+	}
+	
+	class MyMouseAdapter extends MouseAdapter{
+		public void mousePressed(MouseEvent e) {
+			int x=e.getX();
+			int y=e.getY();
+			la.setLocation(x, y);
+		}
+	}
+	
+	public static void main(String[] args) {
+		new MouseAdapterEx();
+	}
+}
+
