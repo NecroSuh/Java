738c802d2d40e68225f5e5e4a8cb31768e8e5821
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-05-08 23:36:24 +0900

    Add files via upload

---
+++ 연습문제/test05/ClickAndDoubleClickEx.java
@@ -0,0 +1,36 @@
+package test05;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class ClickAndDoubleClickEx extends JFrame {
+	public ClickAndDoubleClickEx() {
+		setTitle("Click and DoubleClick ¿¹Á¦");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Container c = getContentPane();
+		c.addMouseListener(new MyMouseListener());
+		setSize(300, 200);
+		setVisible(true);
+	}
+
+	class MyMouseListener extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2) {
+				int r = (int) (Math.random() * 256);
+				int g = (int) (Math.random() * 256);
+				int b = (int) (Math.random() * 256);
+
+				Component c = (Component) e.getSource();
+				c.setBackground(new Color(r, b, g));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new ClickAndDoubleClickEx();
+	}
+
+}
