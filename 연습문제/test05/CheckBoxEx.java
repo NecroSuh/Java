ebaf31597354b8e3dc26ac8d36d278634f851d36
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-05-14 19:09:39 +0900

    Add files via upload

---
+++ 연습문제/test05/CheckBoxEx.java
@@ -0,0 +1,36 @@
+package test05;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class CheckBoxEx extends JFrame {
+	public CheckBoxEx() {
+		setTitle("체크박스 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		ImageIcon cherryIcon = new ImageIcon("C:\\Users\\LG\\Desktop\\images\\cherry.jpg");
+
+		ImageIcon selectedCherryIcon = new ImageIcon("C:\\Users\\LG\\Desktop\\images\\cherry.jpg");
+
+		JCheckBox apple = new JCheckBox("사과");
+		JCheckBox pear = new JCheckBox("배", true);
+		JCheckBox cherry = new JCheckBox("체리", cherryIcon);
+
+		cherry.setBorderPainted(true);
+		cherry.setSelectedIcon(selectedCherryIcon);
+
+		c.add(apple);
+		c.add(pear);
+		c.add(cherry);
+
+		setSize(250, 150);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new CheckBoxEx();
+	}
+}
