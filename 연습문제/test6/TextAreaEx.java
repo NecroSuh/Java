a8e3a2c3558b07e28adcd1b833c73df65319cd27
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-05-16 21:25:32 +0900

    Add files via upload

---
+++ 연습문제/test6/TextAreaEx.java
@@ -0,0 +1,40 @@
+package test6;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class TextAreaEx extends JFrame {
+	private JTextField tf = new JTextField(20);
+	private JTextArea ta = new JTextArea(7, 20);
+
+	public TextAreaEx() {
+		setTitle("텍스트영역 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		c.add(new JLabel("입력 후 <Enter> 키를 입력하세요"));
+		c.add(tf);
+		c.add(new JScrollPane(ta));
+
+		tf.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField) e.getSource();
+				ta.append(t.getText() + "\n");
+				t.setText("");
+			}
+		});
+
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new TextAreaEx();
+	}
+
+}
