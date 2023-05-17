31d5462de76434b571a58833b90e02a40ff17a19
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-05-17 23:35:03 +0900

    Add files via upload

---
+++ 연습문제/test6/ListEx.java
@@ -0,0 +1,39 @@
+package test6;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class ListEx extends JFrame {
+	private String[] fruits = { "apple", "banana", "kiwi", "mango", "pear", "peach", "berry", "strawberry",
+			"blackberry" };
+	private ImageIcon[] images = { new ImageIcon("C:\\Users\\LG\\Desktop\\images\\icon1.png"),
+			new ImageIcon("C:\\Users\\LG\\Desktop\\images\\icon2.png"),
+			new ImageIcon("C:\\Users\\LG\\Desktop\\images\\icon3.png"),
+			new ImageIcon("C:\\Users\\LG\\Desktop\\images\\icon4.png") };
+
+	public ListEx() {
+		setTitle("리스트 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		JList<String> strList = new JList<String>(fruits);
+		c.add(strList);
+
+		JList<ImageIcon> imageList = new JList<ImageIcon>();
+		imageList.setListData(images);
+		c.add(imageList);
+
+		JList<String> scrollList = new JList<String>(fruits);
+		c.add(new JScrollPane(scrollList));
+
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new ListEx();
+	}
+
+}
