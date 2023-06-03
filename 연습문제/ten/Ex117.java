465e3732580851370ae88ed7fc67e01a9469e720
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-06-03 22:34:26 +0900

    Add files via upload

---
+++ 연습문제/ten/Ex117.java
@@ -0,0 +1,36 @@
+package ten;
+
+import javax.swing.*;
+import java.awt.*;
+
+@SuppressWarnings("serial")
+public class Ex117 extends JFrame {
+	private MyPanel panel = new MyPanel();
+
+	public Ex117() {
+		setTitle("격자 그리기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		@SuppressWarnings("unused")
+		private int gridWidth = this.getWidth() / 10;
+		@SuppressWarnings("unused")
+		private int gridHeight = this.getHeight() / 10;
+
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			for (int i = 1; i < 10; i++) {
+				g.drawLine(0, (this.getHeight() / 10) * i, this.getWidth(), (this.getHeight() / 10) * i);
+				g.drawLine((this.getWidth() / 10) * i, 0, (this.getWidth() / 10) * i, this.getHeight());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new Ex117();
+	}
+}
