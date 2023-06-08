014d3c3895734112c1bb9f23b0acda06c6db2894
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-06-08 23:44:52 +0900

    Add files via upload

---
+++ 연습문제/test7/ThreadTimerEx.java
@@ -0,0 +1,50 @@
+package test7;
+
+import javax.swing.*;
+import java.awt.*;
+
+class TimerThread extends Thread {
+	private JLabel timerLabel;
+
+	public TimerThread(JLabel timerLabel) {
+		this.timerLabel = timerLabel;
+	}
+
+	public void run() {
+		int n = 0;
+		while (true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+	}
+}
+
+@SuppressWarnings("serial")
+public class ThreadTimerEx extends JFrame {
+	public ThreadTimerEx() {
+		setTitle("Thread를 상속받은 타이머 스레드 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC, 80));
+		c.add(timerLabel);
+
+		TimerThread th = new TimerThread(timerLabel);
+
+		setSize(300, 170);
+		setVisible(true);
+
+		th.start();
+	}
+
+	public static void main(String[] args) {
+		new ThreadTimerEx();
+	}
+}
