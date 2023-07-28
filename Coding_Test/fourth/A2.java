b353d66afbcb5ac210486933932eda743ca704f6
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-07-28 14:24:03 +0900

    Add files via upload

---
+++ Coding_Test/fourth/A2.java
@@ -0,0 +1,15 @@
+package fourth;
+
+import java.io.*;
+
+public class A2 {
+
+	private void solution() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine() + "\n1");
+	}
+
+	public static void main(String[] args) throws Exception {
+		new A2().solution();
+	}
+}
