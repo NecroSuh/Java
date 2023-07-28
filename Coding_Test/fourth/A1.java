50739bb2365824582465ed2702803ada722d583b
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-07-28 14:19:43 +0900

    Add files via upload

---
+++ Coding_Test/fourth/A1.java
@@ -0,0 +1,19 @@
+package fourth;
+
+import java.io.*;
+
+public class A1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		@SuppressWarnings("unused")
+		StringBuilder sb = new StringBuilder();
+		@SuppressWarnings("unused")
+		StreamTokenizer st;
+
+		@SuppressWarnings("unused")
+		int n = Integer.parseInt(br.readLine());
+
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
