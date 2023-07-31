4082a15fb865a017529852863f9db64c9e4ba863
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-07-31 22:42:08 +0900

    Add files via upload

---
+++ Coding_Test/fifth/Representative_value.java
@@ -0,0 +1,21 @@
+package fifth;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Representative_value {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int sum = 0;
+		int mid = 0;
+		int[] arr = new int[5];
+		for (int i = 0; i < 5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		Arrays.sort(arr);
+		int avg = sum / 5;
+		mid = arr[2];
+		System.out.print(avg + "\n" + mid);
+	}
+}
