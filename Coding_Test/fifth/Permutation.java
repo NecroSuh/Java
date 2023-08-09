9cac5ca68c48d5e7e5679f9f97059516492fb549
NecroSuh <101260920+NecroSuh@users.noreply.github.com>
2023-08-09 22:27:06 +0900

    Add files via upload

---
+++ Coding_Test/fifth/Permutation.java
@@ -0,0 +1,33 @@
+package fifth;
+
+import java.io.*;
+import java.util.*;
+
+public class Permutation {
+	private void solution() throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		boolean[] v = new boolean[n];
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (v[i])
+				continue;
+			cnt++;
+			int s = i;
+			while (!v[s]) {
+				v[s] = true;
+				s = arr[s];
+			}
+		}
+		System.out.println(ccnt == 1 ? 0 : cnt);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Permutation().solution();
+	}
+}
